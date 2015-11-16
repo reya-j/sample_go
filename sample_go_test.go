7fe05899f0ed38a792e67f18fcf573691095021d
buildsample <buildsampletest@gmail.com>
2015-11-16 12:31:09 +0530

    Update sample_go_test.go

--- sample_go_test.go
+++ sample_go_test.go
@@ -7,7 +7,7 @@ import (
 
 var _ = Describe("Sample", func() {
 	Describe("Calculating square root", func() {
-		Context("of the number 4", func() {
+		Context("of the number 41", func() {
 			It("should result 2", func() {
 				Setup()
 				Expect(GetResult()).To(Equal(2.0))
